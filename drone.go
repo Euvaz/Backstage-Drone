3f4c0f4099e6cace43059b064cef442bc33195e8
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2023-02-03 22:13:15 -0600

    feat: Create base enrollment function

--- drone.go
+++ drone.go
@@ -1,10 +1,41 @@
 package main
 
 import (
-    _ "encoding/base64"
-    _ "fmt"
-    _ "github.com/spf13/viper"
-    _ "log"
-    _ "net/http"
-    _ "os"
+	"encoding/base64"
+	"encoding/json"
+	"github.com/spf13/viper"
+	"log"
+	_ "net/http"
+	_ "os"
+
 )
+
+type Token struct {
+    Addr    string  `json:"addr"`
+    Key     string  `json:"key"`
+}
+
+func enroll(tokenEncoded string) {
+    var token Token
+
+    // Decode Base64 token string
+    tokenBytes, err := base64.StdEncoding.DecodeString(tokenEncoded)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    
+    // Convert []Byte type to Token
+    err = json.Unmarshal(tokenBytes, &token)
+    if err != nil {
+        log.Fatalln(err)
+    }
+}
+
+func main() {
+    log.SetFlags(log.Lshortfile)
+    log.SetPrefix("Backstage-Drone: ")
+
+    vi := viper.New()
+    vi.SetConfigFile("config.yaml")
+    vi.ReadInConfig()
+}
