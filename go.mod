8c4fe69eed511d05bf0227b67f72ca4bdb8bc83e
mp4 <90736838+mp4-dev@users.noreply.github.com>
2023-02-05 00:25:21 -0500

    feat: Added HTTP POST request to send token to hive

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/Euvaz/Backstage-Drone
 
 go 1.19
 
-require github.com/spf13/viper v1.15.0
+require (
+	github.com/Euvaz/Backstage-Hive/logger v0.0.0-20230205034344-95cf745ebc0f
+	github.com/spf13/viper v1.15.0
+)
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
@@ -15,6 +18,9 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
-	"github.com/spf13/viper"
+	"fmt"
+	_ "io"
 	"log"
-	_ "net/http"
+	"net/http"
 	_ "os"
 
+	"github.com/Euvaz/Backstage-Hive/logger"
+	"github.com/spf13/viper"
 )
 
 type Token struct {
@@ -21,14 +25,34 @@ func enroll(tokenEncoded string) {
     // Decode Base64 token string
     tokenBytes, err := base64.StdEncoding.DecodeString(tokenEncoded)
     if err != nil {
-        log.Fatalln(err)
+        logger.Fatal(err.Error())
     }
     
     // Convert []Byte type to Token
     err = json.Unmarshal(tokenBytes, &token)
     if err != nil {
-        log.Fatalln(err)
+        logger.Fatal(err.Error())
     }
+
+    // POST request
+    // JSON body
+	body := []byte(fmt.Sprintf(`{
+		"token": %s
+	}`, token.Key))
+
+    // Create a HTTP post request
+	r, err := http.NewRequest("POST", token.Addr, bytes.NewBuffer(body))
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+    r.Header.Add("Content-Type", "application/json")
+    client := &http.Client{}
+    res, err := client.Do(r)
+    if err != nil {
+    	logger.Fatal(err.Error())
+    }
+    defer res.Body.Close()
+
 }
 
 func main() {
