a2bcb0298c51a7ff74f0c0a47e1996e6db3887fb
euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2023-02-05 21:07:29 -0600

    feat: Add command to enroll node
    feat: Add command to enroll node
    
    feat: Add command to enroll drone

--- main.go
+++ main.go
@@ -7,14 +7,58 @@ import (
 	"fmt"
     "net/url"
 	"net/http"
+    "strings"
 
 	"github.com/Euvaz/Backstage-Hive/logger"
+    "github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 type Token struct {
     Addr    string  `json:"addr"`
     Key     string  `json:"key"`
+}
+
+func main() {
+    viper := viper.New()
+    viper.SetConfigFile("config.toml")
+    err :=  viper.ReadInConfig()
+    if err != nil {
+        logger.Fatal(err.Error())
+    }
+
+    viper.SetDefault("name", "drone-1")
+
+    // Add root command
+    cmd := &cobra.Command {
+        Use:   "Backstage-Drone",
+        Short: "Short Desc",
+        Long:  `Long Desc`,
+        PersistentPreRun: func(cmd *cobra.Command, args []string) {
+        },
+        Run: func(cmd *cobra.Command, args []string) {
+            logger.Info("Starting server")
+        },
+    }
+
+    // Add command
+    enrollCmd := &cobra.Command {
+        Use:   "enroll",
+        Short: "Short Desc",
+        Long:  `Long Desc`,
+        Args: cobra.ExactArgs(1),
+        Run: func(cmd *cobra.Command, args []string) {
+            enroll(strings.Join(args, " "))
+        },
+    }
+
+    // Add commands
+    cmd.AddCommand(enrollCmd)
+
+    err = cmd.Execute()
+    if err != nil {
+        logger.Fatal(err.Error())
+    }
 }
 
 func enroll(tokenEncoded string) {
@@ -55,12 +99,4 @@ func enroll(tokenEncoded string) {
     	logger.Fatal(err.Error())
     }
     defer res.Body.Close()
-}
-
-func main() {
-    vi := viper.New()
-    vi.SetConfigFile("config.toml")
-    vi.ReadInConfig()
-
-    //enroll("eyJhZGRyIjoiMTI3LjAuMC4xOjY5NjkiLCJrZXkiOiJ0ZXN0In0=")
 }
